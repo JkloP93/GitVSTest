43aa2cdc0a81617b65070c15a673e29e3971e0cc
JkloP93 <holms1968@mail.ru>
2020-10-28 16:43:23 +0300

    Add project files.

---
+++ GitVSTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitVSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "H#$@el#%lo #Wo$%*&(*@rld";
+
+            var result = string.Join(" ", input.Split(new char[] { ' ' }, StringSplitOptions.None)
+                .Select(x => string.Concat(x.Where(y => Char.IsLetterOrDigit(y)))));
+
+            Console.WriteLine(result);
+        }
+    }
+}
