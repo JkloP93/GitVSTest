c932a06d77fbec11ef838d0eb45911d5f9c93061
JkloP93 <holms1968@mail.ru>
2020-10-30 13:56:14 +0300

    Revert "small fix1"
    This reverts commit c00c481db83f9c00fcecf94c5f390747526d65d3.

--- GitVSTest/Program.cs
+++ GitVSTest/Program.cs
@@ -41,6 +41,8 @@ namespace GitVSTest
 
             Console.WriteLine(RandomTwoDimArr(3, 3));
 
+            Console.WriteLine(NoNumerals("3 is a crowd"));
+
             Console.WriteLine("kek");
         }
 
@@ -100,7 +102,15 @@ namespace GitVSTest
 
             return count;
         }
+        
+        public static string NoNumerals(string input)
+        {
 
-        int smallfix;
+            string[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+            String result = String.Concat(input.Split(new char[] { ' ' }).Select(x => { if (Int32.TryParse(x, out int num) && num < 11) return numbers[num]; return x; }));
+
+            return result;
+        }
     }
 }
